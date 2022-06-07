b32920c8384c3c04ca37c92882556988410d92ff
wandergithub <wandergonzalez0@hotmail.com>
2022-06-07 15:47:36 -0400

    Add check status functionality to categories component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <button type="button">Check status</button>
-  </div>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button type="button" onClick={() => dispatch(checkStatus())}>Check status</button>
+    </div>
+  );
+};
 
 export default Categories;
