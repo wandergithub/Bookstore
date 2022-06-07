fd7fc9acfc3ebabafbada6fcd63e20e55e3f772f
wandergithub <wandergonzalez0@hotmail.com>
2022-06-07 12:05:59 -0400

    Display list of books using the store

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
@@ -5,11 +6,12 @@ import Categories from './components/Categories';
 import ListOfBooks from './components/ListOfBooks';
 
 function App() {
+  const books = useSelector((state) => state.books);
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<ListOfBooks bookList={Books} />} />
+        <Route path="/" element={<ListOfBooks bookList={books} />} />
         <Route path="/Categories" element={<Categories author="Jhon Snow" />} />
       </Routes>
     </div>
