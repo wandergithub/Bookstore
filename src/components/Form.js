2a5f990808c62bf22ba3bdf57e672df6a3706ff8
wandergithub <wandergonzalez0@hotmail.com>
2022-06-07 13:33:44 -0400

    Add add-new-book functionality using dispatch

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-const Form = () => (
-  <div>
-    <form>
-      <h2>Add New Book</h2>
-      <input placeholder="Book Title" type="text" name="title" />
-      <input placeholder="Book Author" type="text" name="Author" />
-      <button type="button">ADD Book</button>
-    </form>
-  </div>
-);
+const Form = () => {
+  const [book, setBook] = useState({});
+  const dispatch = useDispatch();
+  const handleInput = () => {
+    const form = document.getElementById('form');
+    const title = form.elements.title.value;
+    const author = form.elements.author.value;
+    const id = uuidv4();
+    setBook({ title, author, id });
+  };
+
+  return (
+    <div>
+      <form id="form">
+        <h2>Add New Book</h2>
+        <input placeholder="Book Title" type="text" name="title" onChange={handleInput} />
+        <input placeholder="Book Author" type="text" name="author" onChange={handleInput} />
+        <button type="button" onClick={() => dispatch(addBook(book.title, book.author, book.id))}>ADD Book</button>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
